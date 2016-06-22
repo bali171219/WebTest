245b64e56ed4ce385918e661d3500842ac14c45a
bali121791 <lenco@Brandon-Lennoxs-MacBook-Pro.local>
2016-06-22 15:06:25 -0400

    use template instead of string to render homepage

--- src/TDD_Sample/superlists/lists/tests.py
+++ src/TDD_Sample/superlists/lists/tests.py
@@ -8,9 +8,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
-
         response = home_page(request)
 
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do Lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        with open('lists/templates/home.html') as f:
+            expected_content = f.read()
+
+        self.assertEqual(response.content.decode(), expected_content)
