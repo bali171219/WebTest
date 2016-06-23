9f81f60200e082d252c68aeba64cd0c36310d2e9
bali121791 <lenco@Brandon-Lennoxs-MacBook-Pro.local>
2016-06-23 16:29:42 -0400

    Added new unit test to verify that the home page can remember a post request, replaced hard coded row value with dynamic "new_item_text"

--- src/TDD_Sample/superlists/lists/tests.py
+++ src/TDD_Sample/superlists/lists/tests.py
@@ -10,6 +10,17 @@ class HomePageTest(TestCase):
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
         response = home_page(request)
-
         expected_content = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_content)
+
+    def test_home_page_can_remember_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new item'
+
+        response = home_page(request)
+
+        self.assertIn('A new item', response.content.decode())
+
+        expected_content = render_to_string('home.html', {'new_item_text': 'A new item'})
         self.assertEqual(response.content.decode(), expected_content)
