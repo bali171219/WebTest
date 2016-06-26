3c9af3322270c038459542ad3db51bab7e7cd56e
bali171219 <blennox@Brandons-MBP.home>
2016-06-26 18:21:30 -0400

    Created new List Model not yet in proper use

--- src/TDD_Sample/superlists/lists/tests.py
+++ src/TDD_Sample/superlists/lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -30,8 +30,9 @@ class NewListViewTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_lists_page_shows_items_in_database(self):
-        Item.objects.create(text='Item 1')
-        Item.objects.create(text='Item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='Item 1', list=list_)
+        Item.objects.create(text='Item 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -50,16 +51,22 @@ class ListViewTest(TestCase):
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items_to_the_database(self):
+        first_list = List()
+        first_list.save()
         first_item = Item()
         first_item.text = 'Item the first'
+        first_item.list = first_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'second item'
+        second_item.list = first_list
         second_item.save()
 
         first_item_from_db = Item.objects.all()[0]
         self.assertEqual(first_item_from_db.text, 'Item the first')
+        self.assertEqual(first_item_from_db.list, first_list)
 
         second_item_from_db = Item.objects.all()[1]
         self.assertEqual(second_item_from_db.text, 'second item')
+        self.assertEqual(second_item_from_db.list, first_list)
